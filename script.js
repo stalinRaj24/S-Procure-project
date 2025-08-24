8bdce9715e885f8b16d5f4ddbf091ab492660db5
stalinRaj24 <stalin2004raj@gmail.com>
2025-08-24 21:54:50 +0530

    S-procure

---
+++ script.js
@@ -0,0 +1,8 @@
+function showsidebar() {
+    const sidenavbar = document.querySelector(".sidenavbar")
+    sidenavbar.style.display = "flex"
+}
+function closebar() {
+    const sidenavbar = document.querySelector(".sidenavbar")
+    sidenavbar.style.display = "none"
+}
